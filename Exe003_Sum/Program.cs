4d813a6e96be16dd27cae7565f40a79f2f1894e3
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-11-29 00:29:02 +0300

    add random

--- Exe003_Sum/Program.cs
+++ Exe003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 2;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // числа от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // числа от 1 до 9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);

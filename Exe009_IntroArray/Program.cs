51237614ae68cc5f3dc0709d1784cfc931ce90c8
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-01 22:32:22 +0300

    add lesson3

---
+++ Exe009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int [] arrya = {11, 15, 16, 17, 85, 9, 5, 9, 7};
+
+
+int max = Max(
+    Max(arrya[0], arrya[1], arrya[2]),
+    Max(arrya[3], arrya[4], arrya[5]),
+    Max(arrya[6], arrya[7], arrya[8])
+    
+);
+
+Console.WriteLine(max);
---
+++ Exe010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 2, 3, 4, 3, 6, 7, 8, 9};
+
+int n = array.Length;
+
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
